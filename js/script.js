278ecd00e1e6476e35f77bc4336b44441bd90705
Diego63212 <doom63212@gmail.com>
2024-03-07 19:49:41 -0300

    Implement alternative approach

--- js/script.js
+++ js/script.js
@@ -1,20 +1,137 @@
 const display = document.querySelector('.display')
-const calculator = document.querySelector('#calculator-buttons');
+const calculatorBtnDiv = document.querySelector('#calculator-buttons');
 const clearBtn = document.querySelector('.clear')
-const operateBtn = document.querySelector('.operate')
+const ansBtn = document.querySelector('.operate')
 const backspaceBtn = document.querySelector('.backspace')
-const periodBtn = document.querySelector('.dot')
-const buttonsDiv = document.querySelector('.buttons')
+const decimalBtn = document.querySelector('.decimal')
+const buttonsDiv = document.querySelector('.button-container')
 
-let num1 = '';
-let operator = '';
-let num2 = '';
-// Just two stages? three or four should be experimented
-let isFirstNumber = true;
-let result = '';
+// New loop idea: stages //
+/*
+Create new variable named current, main text holder that will pass its value to other stages
+Create new variables numStage1, numStage2
+Create new variable and stage result, branch depending on action: press, clear || press, continue
 
-// THIS IS NOT IDEAL
-periodBtn.disabled = true;
+Pressing buttons should not do anything unless its allowed, buttons should be never disabled
+More individual listener to make code more readable
+*/
+let numStage1 = ''
+let numStage2 = ''
+let operator = ''
+let current = {
+    value: '',
+    saved: '',
+    test: function() {
+        this.saved = this.value;
+        this.value = '';
+    }
+};
+let stage = 0;
+let previousOperator;
+let result = false;
+clearBtn.addEventListener('click', clearListener)
+decimalBtn.addEventListener('click', decimalListener)
+
+calculatorBtnDiv.addEventListener('click', (e) => {
+    e.stopPropagation()
+    if (e.target.nodeName == 'BUTTON') {
+        // STAGE 0 > STAGE 1
+        if (stage == 0) {
+            current.value = '';
+            console.log('STAGE 0>1')
+            stage++
+        }
+        if (e.target.classList.contains('number')) numberListener(e);
+    
+        // Fill with a 0 if nothing is input
+        if (stage == 1) {
+            if (!current.value) {
+                current.value = '0'
+            }
+        }
+        // Trigger after 0 fill
+        if (e.target.classList.contains('operator')) operatorListener(e);
+        /* if (e.target.classList.contains('decimal')) decimalListener(e); */
+    
+        if (stage == 2 && current.value) {
+            current.test()
+        }
+        if (stage == 3) {
+        }
+        // Chain, move result to current value
+        if (stage == 4) {
+            console.log('calculate')
+            current.value = operate(previousOperator, current.saved, current.value)
+            previousOperator = ''
+            current.test()
+            if (result) {
+                stage = 5;
+            } else {
+                stage = 2;
+            }
+        }
+        if (stage == 5) {
+            display.textContent = 'Result: ' + current.saved
+        }
+    
+        // DEBUG DISPLAY
+        display.textContent = `${current.saved} ${operator} ${current.value}`
+    
+        console.log(stage)
+    }
+})
+
+ansBtn.addEventListener('click', ansListener)
+
+function numberListener(e) { 
+    //STAGE 2 > 3
+    if (stage == 2) stage++
+    // STAGE 1 & 2
+    if (stage == 1 || stage == 3) {
+        current.value += e.target.textContent;
+    }
+}
+
+function operatorListener(e) {
+    // STAGE 1 > STAGE 2 || STAGE 3 > STAGE 4
+    if (current.value) {
+        console.log('chain')
+        previousOperator = operator
+        stage++
+    }
+    operator = e.target.textContent
+}
+
+function decimalListener(e) {
+    console.log('decimal', e.type)
+    if (!current.value || current.value.includes('.')) {
+        console.log('decimal: do nothing')
+    } else {
+        current.value += e.target.textContent
+        console.log('decimal: add period')
+    }
+}
+
+function ansListener(e) {
+
+    if (operator && current.value && current.saved) {
+        stage = 4;
+    } else {
+        alert('Incomplete expression')
+    }
+}
+
+function clearListener(e) {
+    e.stopPropagation()
+    stage = 0
+    numStage1 = ''
+    numStage2 = ''
+    operator = ''
+    previousOperator = ''
+    current.value = ''
+    current.test()
+    display.textContent = '0'
+}
 
 function add(num1, num2) {
     return round(+num1 + +num2);
@@ -45,7 +162,7 @@ function operate(operator, num1, num2) {
     if (operator == '/') return (divide(num1, num2) || 'ERROR')
 }
 // Press button on valid keyboard event key, but there has to be a better way with btn animations
-document.addEventListener('keyup', event => {
+/* document.addEventListener('keyup', event => {
     console.log(event.key)
     if (event.key == 'Escape') clearBtn.click();
     if (event.key == 'Backspace') backspaceBtn.click();
@@ -54,7 +171,6 @@ document.addEventListener('keyup', event => {
     if (event.key == '-') document.querySelector('#k-subtract').click();
     if (event.key == '*') document.querySelector('#k-multiply').click();
     if (event.key == '/') document.querySelector('#k-divide').click();
-    if (event.key == '0') document.querySelector('#k-0').click();
     if (event.key == '1') document.querySelector('#k-1').click();
     if (event.key == '2') document.querySelector('#k-2').click();
     if (event.key == '3') document.querySelector('#k-3').click();
@@ -64,116 +180,14 @@ document.addEventListener('keyup', event => {
     if (event.key == '7') document.querySelector('#k-7').click();
     if (event.key == '8') document.querySelector('#k-8').click();
     if (event.key == '9') document.querySelector('#k-9').click();
+    if (event.key == '0') document.querySelector('#k-0').click();
     if (event.key == '.') document.querySelector('#k-dot').click();
-})
-// Main loop, problematic and needs to be spliced
-calculator.addEventListener('click', (event) => {
-    event.stopPropagation()
-    if (event.target.classList.contains('number') && isFirstNumber) {
-        num1 += event.target.textContent;
-        console.log('First:', num1)
-    }
-    
-    if (event.target.classList.contains('operator')) {
-        if (!num1) num1 = '0'
-        if (num1 && num2 && operator) {
-            validateChain()
-        } else if (num1 && !num2) {
-            operator = event.target.textContent;
-            console.log('Operator:', operator)
-            isFirstNumber = false
-        }
-    }
-    
-    if (event.target.classList.contains('number') && !isFirstNumber && operator) {
-        num2 += event.target.textContent;
-        console.log('Second:', num2)
-    }
-    update()
-})
+}) */
 
-operateBtn.addEventListener('click', (event) => {
-    event.stopPropagation();
-    validate()
-});
-
-function validate() {
-    if (operator && num1) {
-        if (!num2) num2 = '0'
-        result = operate(operator, num1, num2);;
-        display.textContent = result;
-        num1 = result
-        num2 = '' 
-        operator = ''
-        console.log('Result:', result)
-        isFirstNumber = true
-    }
-}
-
-function validateChain() {
-    result = operate(operator, num1, num2);;
-    display.textContent = result;
-    num1 = result
-    num2 = ''
-    result = ''
-}
-
-clearBtn.addEventListener('click', (event) => {
-    event.stopPropagation()
-    reset()
-})
-
-backspaceBtn.addEventListener('click', (event) => {
-    event.stopPropagation()
-    backspace()
-})
-
-function reset() {
-    num1 = '';
-    num2 = '';
-    operator = '';
-    display.textContent = '0'
-    isFirstNumber = true;
-    result = ''
-}
-
-function update() {
-    display.textContent = `${num1} ${operator} ${num2}`
-    if (!num1) display.textContent = '0'
-    // PROBLEMATIC, button should never be disabled
-    if (isFirstNumber && num1.includes('.')) {
-        periodBtn.disabled = true;
-    } else {
-        periodBtn.disabled = false;
-    }
-    // PROBLEMATIC, button should never be disabled
-    if (!isFirstNumber && num2 && num2.includes('.')) {
-        periodBtn.disabled = true;
-    } else if (!isFirstNumber) {
-        periodBtn.disabled = false;
-    }
-    checkDot()
-}
-
-function backspace() {
+/* function backspace() {
     if (isFirstNumber) {
         num1 = num1.slice(0, num1.length - 1)
     } else {
         num2 = num2.slice(0, num2.length - 1)
     }
-    update()
-}
-// PROBLEMATIC, button should never be disabled
-function checkDot () {
-    if (isFirstNumber && !num1 || num1.includes('.')) {
-        periodBtn.disabled = true;
-    } else {
-        periodBtn.disabled = false;
-    }
-
-    if (!isFirstNumber && !num2 || num2.includes('.')) {
-        periodBtn.disabled = true;
-    } else if (!isFirstNumber) {
-        periodBtn.disabled = false;
-    }
-}
+} */
