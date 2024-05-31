28383c5231fcff67f7c29543fc9bb94862c18f2e
Diego63212 <doom63212@gmail.com>
2024-05-30 22:41:24 -0300

    Simplify JS code
    * Convert number object to simpler elements.
    * Move button event listener to bubbling event.
    * Move error display to round()fn.
    * Move math operation check to operate()fn.
    * Better button .active class toggle.
    * Added modularClear()fn to handle storing.
    * Change some code to single line when possible.

--- js/script.js
+++ js/script.js
@@ -1,109 +1,89 @@
 const display = document.querySelector('.display');
 const calculatorBtnDiv = document.querySelector('#calculator-buttons');
-const clearBtn = document.querySelector('.clear');
-const ansBtn = document.querySelector('.operate');
-const backspaceBtn = document.querySelector('.backspace');
 const decimalBtn = document.querySelector('.decimal');
-
 let operator = '';
-let number = {
-    current: '',
-    previous: '',
-    store: function () {
-        this.previous = this.current;
-        this.current = '';
-    },
+let currentOperand = '';
+let previousOperand = '';
+
+function storeOperand() {
+    previousOperand = currentOperand;
+    modularClear(true);
 };
-
-clearBtn.addEventListener('click', () => {
+// Clear
+function modularClear(saveStoredOperand) {
+    if (!saveStoredOperand) previousOperand = '';
     operator = '';
-    number.current = '';
-    number.previous = '';
-});
+    currentOperand = '';
+};
+// Adds a real number zero
+function zeroFill() {if (!currentOperand) currentOperand = '0'};
 
-ansBtn.addEventListener('click', () => {
-    if (number.previous) {
-        number.current = operate(operator, number.previous, number.current);
-        number.previous = '';
-        operator = '';
-    }
-});
-
-backspaceBtn.addEventListener('click', () => number.current = number.current.slice(0, number.current.length - 1));
 decimalBtn.addEventListener('click', () => {
-    if (!number.current.includes('.')) {
+    if (!currentOperand.includes('.')) {
         zeroFill();
-        number.current += '.';
-    }
-});
-
-function zeroFill() {
-    if (!number.current) {
-        number.current = '0';
+        currentOperand += '.';
     };
-}
+});
+// Listen to bubbling click events from buttons with classes inside this div. Update display on interaction
+calculatorBtnDiv.addEventListener('click', (e) => {
+    if (e.target.classList.contains('number')) currentOperand += e.target.textContent;
+    if (e.target.classList.contains('operator')) operatorCheck(e);
+    if (e.target.classList.contains('operate')) operate();
+    if (e.target.classList.contains('clear')) modularClear();
+    if (e.target.classList.contains('backspace')) currentOperand = currentOperand.slice(0, currentOperand.length - 1);
+    display.textContent = `${previousOperand} ${operator} ${currentOperand || '0'}`;
+    if (currentOperand === 'ERROR') currentOperand = '';
+});
 // Makes sure expected action is taken when a user clicks and chains calculations
 function operatorCheck(e) {
-    if (number.current && !number.previous) {
-        number.store();
-    } else if (!number.current && !number.previous) {
+    if (currentOperand && !previousOperand) {
+        storeOperand();
+    } else if (!currentOperand && !previousOperand) {
         zeroFill();
-        number.store();
-    } else if (number.current && number.previous) {
-        number.current = operate(operator, number.previous, number.current);
-        number.store();
-    }
+        storeOperand();
+    } else if (currentOperand && previousOperand) {
+        operate();
+        storeOperand();
+    };
     operator = e.target.textContent;
-}
-// Listen to bubbling click events from buttons with class number/operator inside this div. Also update display
-calculatorBtnDiv.addEventListener('click', (e) => {
-    if (e.target.classList.contains('number')) {number.current += e.target.textContent};
-    if (e.target.classList.contains('operator')) operatorCheck(e);
-    display.textContent = `${number.previous} ${operator} ${number.current || '0'}`;
-    if (number.current == 'ERROR') number.current = ''
-});
-
-function add(num1, num2) {
-    return round(+num1 + +num2);
-}
-function subtract(num1, num2) {
-    return round(num1 - num2);
-}
-function multiply(num1, num2) {
-    return round(num1 * num2);
-}
-function divide(num1, num2) {
-    if (num2 == 0) {
+};
+// Select correct math action and store its result
+function operate() {
+    let result;
+    if (previousOperand && operator === '+') result = add();
+    if (previousOperand && operator === '-') result = subtract();
+    if (previousOperand && operator === '*') result = multiply();
+    if (previousOperand && operator === '/') result = divide();
+    modularClear()
+    currentOperand = round(result);
+};
+// Math
+function add() {return +previousOperand + +currentOperand};
+function subtract() {return previousOperand - currentOperand};
+function multiply() {return previousOperand * currentOperand};
+function divide() {
+    if (currentOperand == false) {
         alert('What are you doing???');
     } else {
-        return round(num1 / num2);
-    }
-}
-// Rounds the resulting number to three decimal places
+        return round(previousOperand / currentOperand);
+    };
+};
+// Rounds the number to three decimal places
 function round(result) {
-    return (Math.round(result * 1000) / 1000);
-}
-
-function operate(operator, num1, num2) {
-    if (operator == '+') return (add(num1, num2));
-    if (operator == '-') return (subtract(num1, num2));
-    if (operator == '*') return (multiply(num1, num2));
-    if (operator == '/') return (divide(num1, num2) || 'ERROR');
+    return (Math.round(result * 1000) / 1000) || 'ERROR';
 }
 // Uses getElementById because it doesn't return an error on invalid id like querySelector
 document.addEventListener('keydown', e => {
-    let element = document.getElementById(`kb:${e.key}`)
+    const element = document.getElementById(`kb:${e.key}`);
     if (element) {
-        element.click()
+        element.click();
         if (!element.classList.contains('active')) {
-            element.className += ' active'
-        }
-    }
+            element.classList.toggle('active');
+        };
+    };
 });
 
 document.addEventListener('keyup', e => {
-    let element = document.getElementById(`kb:${e.key}`)
-    if (element) {
-        element.className = element.className.replace(' active', '')
-    }
-})
+    const element = document.getElementById(`kb:${e.key}`);
+    if (element) element.classList.toggle('active');
+});
