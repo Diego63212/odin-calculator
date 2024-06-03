a9ee21a84c2170e65fba332025c2735d13332b7d
Diego63212 <doom63212@gmail.com>
2024-06-03 19:43:26 -0300

    Reduce code amount
    * Don't allow leading zeros
    * Fix error result when pressing ans
    * Changed logic to always have a real zero and remove zeroFill()
    * Moved button listener to main bubbling event listener
    * Simplified operatorCheck() code

--- js/script.js
+++ js/script.js
@@ -1,72 +1,63 @@
 const display = document.querySelector('.display');
 const calculatorBtnDiv = document.querySelector('#calculator-buttons');
-const decimalBtn = document.querySelector('.decimal');
 let operator = '';
-let currentOperand = '';
+let currentOperand = '0';
 let previousOperand = '';
 
+// Math operations
+let add = () => +previousOperand + +currentOperand;
+let subtract = () => previousOperand - currentOperand;
+let multiply = () => previousOperand * currentOperand;
+let divide = () => previousOperand / currentOperand;
+let round = result => Math.round(result * 1000) / 1000; // Rounds the number to three decimal places
+// Select correct math operation and store its result
+function operate() {
+    let result;
+    if (operator === '+') result = add();
+    if (operator === '-') result = subtract();
+    if (operator === '*') result = multiply();
+    if (operator === '/' && currentOperand > '0') result = divide(); // Don't compute division by zero
+    modularClear();
+    currentOperand = (result) ? round(result) : 'ERROR'; // Return error on empty result (division by zero)
+};
+// Saves number or result for future use
 function storeOperand() {
     previousOperand = currentOperand;
     modularClear(true);
 };
-// Clear
+// Clear variables with optional saving
 function modularClear(saveStoredOperand) {
-    if (!saveStoredOperand) previousOperand = '';
+    if (!saveStoredOperand) previousOperand = ''; // Saves result when chaining math operations
     operator = '';
-    currentOperand = '';
+    currentOperand = '0';
 };
-// Adds a real number zero
-function zeroFill() {if (!currentOperand) currentOperand = '0'};
-
-decimalBtn.addEventListener('click', () => {
-    if (!currentOperand.includes('.')) {
-        zeroFill();
-        currentOperand += '.';
-    };
-});
-// Listen to bubbling click events from buttons with classes inside this div. Update display on interaction
-calculatorBtnDiv.addEventListener('click', (e) => {
-    if (e.target.classList.contains('number')) currentOperand += e.target.textContent;
-    if (e.target.classList.contains('operator')) operatorCheck(e);
-    if (e.target.classList.contains('operate')) operate();
-    if (e.target.classList.contains('clear')) modularClear();
-    if (e.target.classList.contains('backspace')) currentOperand = currentOperand.slice(0, currentOperand.length - 1);
-    display.textContent = `${previousOperand} ${operator} ${currentOperand || '0'}`;
-    if (currentOperand === 'ERROR' || previousOperand === 'ERROR') modularClear();
-});
 // Makes sure expected action is taken when a user clicks and chains calculations
 function operatorCheck(e) {
-    if (currentOperand && !previousOperand) {
-        storeOperand();
-    } else if (!currentOperand && !previousOperand) {
-        zeroFill();
-        storeOperand();
-    } else if (currentOperand && previousOperand) {
+    if (!(currentOperand === '0') && previousOperand) {
         operate();
+        storeOperand();
+    } else if (!previousOperand) {
         storeOperand();
     };
     operator = e.target.textContent;
 };
-// Select correct math action and store its result
-function operate() {
-    let result;
-    if (previousOperand && operator === '+') result = add();
-    if (previousOperand && operator === '-') result = subtract();
-    if (previousOperand && operator === '*') result = multiply();
-    if (previousOperand && operator === '/') result = divide();
-    modularClear();
-    if (Number.isFinite(result)) return currentOperand = round(result);
-    currentOperand = 'ERROR';
-};
-// Math
-function add() {return +previousOperand + +currentOperand};
-function subtract() {return previousOperand - currentOperand};
-function multiply() {return previousOperand * currentOperand};
-function divide() {return previousOperand / currentOperand};
-// Rounds the number to three decimal places
-function round(result) {
-    return (Math.round(result * 1000) / 1000);
-}
+// Listen to bubbling click events from buttons with classes inside this div. Update display after interaction
+calculatorBtnDiv.addEventListener('click', (e) => {
+    if (currentOperand === 'ERROR' || previousOperand === 'ERROR') modularClear();
+    if (e.target.classList.contains('number')) {
+        if (currentOperand === '0') currentOperand = '';
+        currentOperand += e.target.textContent;
+    };
+    if (e.target.classList.contains('decimal')) {
+        if (!currentOperand.includes('.')) currentOperand += '.';
+    };
+    if (e.target.classList.contains('operator')) operatorCheck(e);
+    if (e.target.classList.contains('operate') && previousOperand) operate();
+    if (e.target.classList.contains('clear')) modularClear();
+    if (e.target.classList.contains('backspace')) currentOperand = currentOperand.slice(0, currentOperand.length - 1);
+    if (currentOperand === '') currentOperand = '0'; // Safeguard against invalid operations
+    display.textContent = `${previousOperand} ${operator} ${currentOperand}`;
+});
 // Uses getElementById because it doesn't return an error on invalid id like querySelector
 document.addEventListener('keydown', e => {
     const element = document.getElementById(`kb:${e.key}`);
