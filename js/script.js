6e0404dde6153267415687877a03b76d82b9358f
Diego63212 <doom63212@gmail.com>
2024-06-07 00:20:28 -0300

    Further reduce code
    * Make operate() return the value and change its name to getResult()
    * Merge storeOperand() and modularClear() to smartClear().
    * Simplify operatorCheck() with the merged fn() from above comment.
    * Change round() to return a string.
    * Change some conditional expressions.
    * Prevent division by zero calculation.
    * Improve code comments.

--- js/script.js
+++ js/script.js
@@ -8,55 +8,46 @@ let hasClass = (e, className) => e.target.classList.contains(className); // Chec
 let add = () => +previousOperand + +currentOperand;
 let subtract = () => previousOperand - currentOperand;
 let multiply = () => previousOperand * currentOperand;
-let divide = () => previousOperand / currentOperand;
-let round = result => Math.round(result * 1000) / 1000; // Rounds the number to three decimal places
+let divide = () => (currentOperand !== '0') ? previousOperand / currentOperand : 'ERROR'; // Don't compute division by zero
+let round = result => (Math.round(result * 1000) / 1000).toString(); // Rounds the number to three decimal places
 // Select correct math operation and store its result
-function operate() {
+function getResult() {
     let result;
     if (operator === '+') result = add();
     if (operator === '-') result = subtract();
     if (operator === '*') result = multiply();
-    if (operator === '/' && currentOperand > '0') result = divide(); // Don't compute division by zero
-    modularClear();
-    currentOperand = (Number.isFinite(result)) ? round(result).toString() : 'ERROR'; // Error on Infinity (division by zero)
+    if (operator === '/') result = divide();
+    smartClear();
+    return (result !== 'ERROR') ? round(result) : result; // Rounds the result only if it is not an error
 };
-// Saves number or result for future use
-function storeOperand() {
-    previousOperand = currentOperand;
-    modularClear(true);
-};
-// Clear variables with optional saving
-function modularClear(saveStoredOperand) {
-    if (!saveStoredOperand) previousOperand = ''; // Saves result when chaining math operations
+// Clear variables with optional saving for chain operations
+function smartClear(saveValue, value) {
+    saveValue ? previousOperand = value : previousOperand = '';
     operator = '';
     currentOperand = '0';
 };
 // Makes sure expected action is taken when a user clicks and chains calculations
 function operatorCheck(e) {
-    if (!(currentOperand === '0') && previousOperand) {
-        operate();
-        storeOperand();
-    } else if (!previousOperand) {
-        storeOperand();
+    if (!previousOperand) {
+        smartClear(true, currentOperand);
+    } else if (currentOperand !== '0') {
+        smartClear(true, getResult());
     };
     operator = e.target.textContent;
 };
-// Listen to bubbling click events from buttons with classes inside this div. Update display after interaction
+// Listen to bubbling click events from buttons with classes inside this div.
 calculatorBtnDiv.addEventListener('click', (e) => {
-    if (currentOperand === 'ERROR' || previousOperand === 'ERROR') modularClear();
+    if (currentOperand === 'ERROR') smartClear(); // Clear everything if ERROR is in one of the variables
     if (hasClass(e, 'number')) {
-        if (currentOperand === '0') currentOperand = '';
-        currentOperand += e.target.textContent;
-    };
-    if (hasClass(e, 'decimal')) {
-        if (!currentOperand.includes('.')) currentOperand += '.';
-    };
+        (currentOperand !== '0') ? currentOperand += e.target.textContent : currentOperand = e.target.textContent;
+    }
+    if (hasClass(e, 'decimal') && !currentOperand.includes('.')) currentOperand += '.'; // Add only one decimal point
     if (hasClass(e, 'operator')) operatorCheck(e);
-    if (hasClass(e, 'operate') && previousOperand) operate();
-    if (hasClass(e, 'clear')) modularClear();
+    if (hasClass(e, 'operate') && previousOperand) currentOperand = getResult();
+    if (hasClass(e, 'clear')) smartClear();
     if (hasClass(e, 'backspace')) currentOperand = currentOperand.slice(0, currentOperand.length - 1);
     if (currentOperand === '') currentOperand = '0'; // Safeguard against invalid states
-    display.textContent = `${previousOperand} ${operator} ${currentOperand}`;
+    display.textContent = `${previousOperand} ${operator} ${currentOperand}`; // Update display after interaction
 });
 // Uses getElementById because it doesn't return an error on invalid id like querySelector
 document.addEventListener('keydown', e => {
