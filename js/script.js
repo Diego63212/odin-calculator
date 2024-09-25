9d830bfa0d58636c9fd8e8ef5dc8fa4504a104cc
Diego63212 <doom63212@gmail.com>
2024-09-25 16:56:13 -0300

    Less code 2
    * Simplify text slice safeguard.
    * Unindent keydown event code.
    * Change some fn() into constants.
    * Move some code around.

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 const display = document.querySelector('.display');
 const calculatorBtnDiv = document.querySelector('#calculator-buttons');
+const hasClass = (e, className) => e.target.classList.contains(className); // Check element class short version
 let operator = previousOperand = currentOperand = '';
-let hasClass = (e, className) => e.target.classList.contains(className); // Check element class short version
 // Math operations
-let add = () => +previousOperand + +currentOperand;  // Prevent string concatenation
-let subtract = () => previousOperand - currentOperand;
-let multiply = () => previousOperand * currentOperand;
-let divide = () => (currentOperand !== '0') ? previousOperand / currentOperand : 'ERROR'; // Don't compute division by zero
-let round = result => (Math.round(result * 1000) / 1000).toString(); // Rounds the number to three decimal places
+const add = () => +previousOperand + +currentOperand;  // Prevent string concatenation
+const subtract = () => previousOperand - currentOperand;
+const multiply = () => previousOperand * currentOperand;
+const divide = () => (currentOperand !== '0') ? previousOperand / currentOperand : 'ERROR'; // Don't compute division by zero
+const round = result => (Math.round(result * 1000) / 1000).toString(); // Rounds the number to three decimal places
 // Select correct math operation and store its result
 function getResult() {
     let result;
@@ -17,31 +17,30 @@ function getResult() {
     if (operator === '/') result = divide();
     smartClear();
     return (result !== 'ERROR') ? round(result) : result; // Round the result only if valid result exists
-};
+}
 // Clear variables with optional saving for chain operations
 function smartClear(saveValue) {
-    saveValue ? previousOperand = saveValue : previousOperand = '';
+    (saveValue) ? previousOperand = saveValue : previousOperand = '';
     operator = '';
     currentOperand = '0';
-};
+}
 // Makes sure expected action is taken when a user clicks and chains calculations
-function operatorCheck(e) {
+function operatorCheck(btn) {
     if (!previousOperand) {
         smartClear(currentOperand);
     } else if (currentOperand !== '0') {
         smartClear(getResult());
-    };
-    operator = e.target.textContent;
-};
+    }
+    operator = btn.textContent;
+}
 // Listen to bubbling click events from buttons with classes inside this div.
 calculatorBtnDiv.addEventListener('click', (e) => {
     if (currentOperand === 'ERROR') smartClear(); // Clear everything if ERROR is in one of the variables
     if (hasClass(e, 'number')) {
         (currentOperand !== '0') ? currentOperand += e.target.textContent : currentOperand = e.target.textContent;
     }
-    if (hasClass(e, 'backspace')) currentOperand = currentOperand.slice(0, currentOperand.length - 1);
-    if (currentOperand === '') currentOperand = '0'; // Safeguard against invalid states
-    if (hasClass(e, 'operator')) operatorCheck(e);
+    if (hasClass(e, 'backspace')) currentOperand = currentOperand.slice(0, currentOperand.length - 1) || '0';
+    if (hasClass(e, 'operator')) operatorCheck(e.target);
     if (hasClass(e, 'decimal') && !currentOperand.includes('.')) currentOperand += '.'; // Add only one decimal point
     if (hasClass(e, 'operate') && previousOperand) currentOperand = getResult();
     if (hasClass(e, 'clear')) smartClear();
@@ -50,15 +49,11 @@ calculatorBtnDiv.addEventListener('click', (e) => {
 // Uses getElementById because it doesn't return an error on invalid id like querySelector
 document.addEventListener('keydown', e => {
     const element = document.getElementById(`kb:${e.key}`);
-    if (element) {
-        element.click();
-        if (!hasClass(e, 'active')) {
-            element.classList.toggle('active');
-        };
-    };
+    if (!element) return;
+    if (!(element.classList.contains('active'))) element.classList.toggle('active');
+    element.click();
 });
-
 document.addEventListener('keyup', e => {
     const element = document.getElementById(`kb:${e.key}`);
-    if (element) element.classList.toggle('active');
+    if (element.classList.contains('active')) element.classList.toggle('active');
 });
